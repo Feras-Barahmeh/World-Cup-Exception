fa862e496b817ce3fc3294182d98b57e3a354ffa
Feras-Barahmmeh <ferasbarahmeh55@gmail.com>
2023-05-27 20:52:39 +0300

    Add README file

--- main.py
+++ main.py
@@ -1,10 +1,3 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import matplotlib.ticker as ticker
-import matplotlib.ticker as plticker
-
 from prepareDataset import *
 from WorldCupExpectationModel import *
 
